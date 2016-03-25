efe0c1e7a640ce778c9bd4b5b75ffda8b0368c72
abdulaliumich <abdulali@umich.edu>
2016-03-25 17:33:41 -0400

    added guestbook outline

---
+++ db/db_create.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from models import Base
+
+Base.metadata.create_all()
--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class MainHandler(webapp2.RequestHandler):
 
         current_page = ""
         
-        #For now, the only pathnames that will succeed in the try-block are "/about", "/places", "/contact"
+        #For now, the only pathnames that will succeed in the try-block are "/about", "/places", "/contact", "/guestbook",
         #and "/index".
         #All other pathnames (e.g. "/", "/about.html", "/blhahlsdu") should enter
         #the except-block and be redirected to the index page.
@@ -39,7 +39,7 @@ class MainHandler(webapp2.RequestHandler):
             if self.request.path.islower() == False:
                 raise Exception ('pathname must be all lower case')
             template = JINJA_ENVIRONMENT.get_template("templates/" + self.request.path + ".html")
-            #current_page should end up being one of the following four values: "INDEX", "ABOUT", "PLACES", "CONTACT"
+            #current_page should end up being one of the following four values: "INDEX", "ABOUT", "PLACES", "CONTACT", "GUESTBOOK"
             current_page = self.request.path[1:].upper()
         except:
             logging.info("Redirecting to index.html")

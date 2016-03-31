6c29b4d21c1bb19a6631ede65dbf440842dfc34f
abdulaliumich <abdulali@umich.edu>
2016-03-30 21:31:20 -0400

    add new place images and twitter buttons

---
+++ static/twitter.js
@@ -0,0 +1,18 @@
+//Source code for Twitter buttons on the Places page
+//References: twitter api documentation and https://www.thesocialmediahat.com/article/how-attach-images-tweet-buttons
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));

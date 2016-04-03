54445dc22db4d9bae1605d23b214887becf71ffa
abdulaliumich <abdulali@umich.edu>
2016-04-03 13:59:45 -0400

    Some footer styling

--- static/js/main.js
+++ static/js/main.js
@@ -47,10 +47,11 @@ function loginButton(loginId, logoutId, loggedIn) {
 	}
 };
 
+//jQuery stuff
 
-
+//When the page loads
 $( document ).ready(function() {
-	console.log("here");
+	console.log("Hello!");
 	//Change navbar from nav-pills to nav-navbar based on the screen size of the device
     if ($(window).width() < 768) {
     	$('#my-navbar').removeClass('nav-pills').addClass('navbar-nav');
@@ -60,6 +61,7 @@ $( document ).ready(function() {
     }
 });
 
+//change nav class when a user resizes the screen
 $(window).on('resize', function(){
       var win = $(this);
       if (win.width() >= 768) {
